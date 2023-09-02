a37920cbf3977340d9fc01acdb3763a6cee6910d
namelew <diogomaciel.cunha@gmail.com>
2023-09-01 21:44:49 -0300

    Movendo a tela de cadastro para o diretorio de paginas e criando a rota no router

--- Frontend/src/router.js
+++ Frontend/src/router.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
+import Cadastro from './pages/Cadastro';
 
 const AppRouter = () => {
     return (
         <Router>
             <Routes>
                 <Route path='/login' element={<Login />}/>
+                <Route path='/cadastro' element={<Cadastro />}/>
             </Routes>
         </Router>
     );
