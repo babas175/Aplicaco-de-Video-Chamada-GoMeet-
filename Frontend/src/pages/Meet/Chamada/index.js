0f678bd15f7c57ca6f2fa94ea514bdaec4898cf3
Cecilia-Penha <ceciliapenha3@gmail.com>
2023-10-03 15:13:00 -0300

    Iniciando página de chamada

--- Frontend/src/pages/Meet/Chamada/index.js
+++ Frontend/src/pages/Meet/Chamada/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Chamada.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faClose,faVideoCamera,faMicrophone,faBars} from '@fortawesome/free-solid-svg-icons';
 
 function Chamada(){
     return(
         <>
-            
+            <div className='video-container'>
+            </div>
+            <div className='botoes'>
+                <button className='botao'><FontAwesomeIcon icon={faVideoCamera} /></button>
+                <button className='botao'><FontAwesomeIcon icon={faMicrophone} /></button>
+                <button className='botao'><FontAwesomeIcon icon={faClose} /></button>
+                <button className='botao-op'><FontAwesomeIcon icon={faBars} /></button>
+            </div>
         </>
     );
 }
